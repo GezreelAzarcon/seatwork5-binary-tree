f1fdf6d1e7ffd5775a83431e1ece05be65150008
GezreelAzarcon <gezreelwazarcon@gmail.com>
2023-01-14 12:23:44 +0800

    Created a simple guideline for creating the binary tree

---
+++ binaryTree.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+#Create a binary tree class
+#Create a method for __init__ with main class variables
+#Create a method for adding a new node
+#Create a traversal method for sorting the nodes
+#Create a method for building the binary tree
